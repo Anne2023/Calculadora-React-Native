e16d249cb66660715b7adc86d733a3dcf5179118
Amábile <amabile.analistadesistemas@gmail.com>
2023-11-13 20:06:22 -0300

    feat: criação do TextImput

--- CalculadoraReactNative/App.js
+++ CalculadoraReactNative/App.js
@@ -1,10 +1,16 @@
-import React, {useStae} from "react";
+import React, {useState} from "react";
 import { SafeAreaView, StyleSheet,View, Text, TouchableHighlight, TextInput } from "react-native";
 
 export default function App() {
+
+  const[valor, setValor]=useState(0)
   return (
     <SafeAreaView>
       <Text style={styles.title}>Calculadora</Text>
+    <TextInput
+    value={string(valor)}
+    > 
+    </TextInput>
     </SafeAreaView>
   );
 };
