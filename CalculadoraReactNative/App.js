9e00829e3ea17801a0477026f8b1d59f84ebc117
Amábile <amabile.analistadesistemas@gmail.com>
2023-11-13 19:56:57 -0300

    style: formatação do código

--- CalculadoraReactNative/App.js
+++ CalculadoraReactNative/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useStae} from "react";
+import { SafeAreaView, StyleSheet,View, Text, TouchableHighlight, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Text style={styles.title}>Calculadora</Text>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const estilos = StyleSheet.create({
+
 });
